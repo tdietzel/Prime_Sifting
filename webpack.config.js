cf20422b11cb698238a0d30cac05ea79f2a10008
tdietzel22 <147360049+tdietzel22@users.noreply.github.com>
2023-11-27 15:07:41 -0800

    Update file name

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
     new ESLintPlugin(),
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: "Shape Tracker",
+      title: "Prime Sifting",
       template: "./src/index.html",
       inject: "body",
     }),
