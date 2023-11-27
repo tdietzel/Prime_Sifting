35d7f824686aee29931e57cea4118089e955f9ba
tdietzel22 <147360049+tdietzel22@users.noreply.github.com>
2023-11-27 15:06:18 -0800

    Update complete Test #3

--- src/sifter.js
+++ src/sifter.js
@@ -4,11 +4,28 @@ export const sifter = (input) => {
   } else if (input < 3) {
     return "Error: Number must be greater than 2."
   }
+
   const countUpArray = [];
   for (let i = 2; i <= input; i++) {
-    if (!(i % 2 === 0)){
-      countUpArray.push(i);
+    countUpArray.push(i);
+  }
+  // console.log(`CountUpArray: ${countUpArray}`);
+  const arrayBuilder = (currentPrime,currentArray) => {
+    const newArray = [...currentArray];
+    newArray.forEach((number) => {
+      if ((number % currentPrime === 0) && !(number === currentPrime)){ 
+        const index = newArray.indexOf(number);
+        newArray.splice(index, 1);
+      }
+    });
+    const newPrimeLocation = newArray.indexOf(currentPrime);
+    const newPrime = newArray[newPrimeLocation + 1];
+    if (currentArray.toString() === newArray.toString()) {
+      return newArray;
+    } else {
+      return arrayBuilder(newPrime,newArray);
     }
   }
-  return countUpArray;
+  
+  return arrayBuilder(2,countUpArray);
 }
