2ec14866e28b069ab9c63478b3a4e83aaa24538c
tdietzel22 <147360049+tdietzel22@users.noreply.github.com>
2023-11-27 13:29:41 -0800

    Add complete Test #3 for sifter
    Co-authored-by: Aaron Mejia <mejia-dev@users.noreply.github.com>

--- __tests__/sifter.test.js
+++ __tests__/sifter.test.js
@@ -9,4 +9,9 @@ describe (sifter, () => {
   test("It will return an error if the inputted number is not greater than 2", () => {
     expect(sifter(2)).toEqual("Error: Number must be greater than 2.");
   });
+
+  test("It will create a list of numbers starting from 2 up to the number that was inputted and remove all multiples of two from the list", () => {
+    expect(sifter(5)).toEqual([3,5]);
+  });
+
 });
--- src/sifter.js
+++ src/sifter.js
@@ -4,4 +4,11 @@ export const sifter = (input) => {
   } else if (input < 3) {
     return "Error: Number must be greater than 2."
   }
+  const countUpArray = [];
+  for (let i = 2; i <= input; i++) {
+    if (!(i % 2 === 0)){
+      countUpArray.push(i);
+    }
+  }
+  return countUpArray;
 }
